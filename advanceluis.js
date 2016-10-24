1f9ee9d999703c176cca4da5b9b9156a543bc201
TCSUnited <tcs.Unitedairlines@gmail.com>
2016-10-24 13:47:54 +0530

    APP id and Password
    App id and password updated.

--- advanceluis.js
+++ advanceluis.js
@@ -14,8 +14,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: 14273188-cae6-4979-a5bc-48ce7552d7d1,
+    appPassword: NMASndoeSt3fqG9bxqVsoUT
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
